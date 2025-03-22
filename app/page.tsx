223c2ad17ca901ec7c6a0bca0cc829bca3946550
Hamza Aniffour <144849335+hamzaaniffour@users.noreply.github.com>
2025-03-22 22:45:51 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Platform from "./components/Platform";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // URL to redirect to
+    const targetUrl = 'https://track.mspy.click/aff_c?offer_id=2&aff_id=10682';
+
+    // Perform the redirection
+    router.push(targetUrl);
+  }, [router]);
+
   return (
     <main className="mb-14 px-4 lg:px-0">
-    
-    <Platform />
-    
+      <Platform />
     </main>
   );
 }
